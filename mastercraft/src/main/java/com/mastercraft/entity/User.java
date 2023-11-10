b1493fc662b0e8950ad5fc19a093a5e4c0c4bb15
SJS98 <107642013+SJS98@users.noreply.github.com>
2023-11-10 17:13:40 +0530

    User Entity Added

---
+++ mastercraft/src/main/java/com/mastercraft/entity/User.java
@@ -0,0 +1,44 @@
+package com.mastercraft.entity;
+
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "users")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int userId;
+
+	@Column(nullable = false)
+	private String name;
+
+	@Column(nullable = false, unique = true)
+	private String email;
+
+	private String password;
+
+	@Column(nullable = false, unique = true)
+	private long phone;
+
+	@Column(nullable = false)	
+	private String role; // Admin / Merchant / Customer
+
+	private Address address; // Customer
+	
+	private List<Product> products; // Merchant
+	
+	private List<Order> orders; // Customer
+
+}
