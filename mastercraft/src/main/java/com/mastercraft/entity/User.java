da0270c5373535a5e98de8e1f27fb0ac1797bd4b
Sridhar-venkata <bandasridhar2002@gmail.com>
2023-11-11 12:04:15 +0530

    Your commit message here

--- mastercraft/src/main/java/com/mastercraft/entity/User.java
+++ mastercraft/src/main/java/com/mastercraft/entity/User.java
@@ -28,7 +28,7 @@ public class User {
 
 	@Column(nullable = false, unique = true)
 	private String email;
-
+	@Column(nullable = false)
 	private String password;
 
 	@Column(nullable = false, unique = true)
