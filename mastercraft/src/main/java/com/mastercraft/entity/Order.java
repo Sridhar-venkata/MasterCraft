8661c6a79b2b82a661372107801d4db75896779b
Sridhar-venkata <bandasridhar2002@gmail.com>
2023-11-11 15:04:29 +0530

    Updated entities and user dao methods

--- mastercraft/src/main/java/com/mastercraft/entity/Order.java
+++ mastercraft/src/main/java/com/mastercraft/entity/Order.java
@@ -15,8 +15,12 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Setter
+@Getter
 @Table(name="orders")
 public class Order {
 	
